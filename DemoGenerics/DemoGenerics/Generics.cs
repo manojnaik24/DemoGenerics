c294938ec13a538b23223811d5dee749246893c5
Manoj naik <chittinaik@gamil.com>
2023-09-27 09:02:04 +0530

    [Manoj] add:Generics

---
+++ DemoGenerics/DemoGenerics/Generics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGenerics
+{
+    //Generrics is a single form 
+    //It does not specify a particular type
+    //Generic allow us to a single class or method that ca be used for different data types.
+    //<T> with type parameter
+    //type parameter
+    internal class Generics<T>
+    {
+        //public string data;
+        public T data;
+        public Generics(T data)
+        {
+            this.data = data;
+            Console.WriteLine(this.data);
+        }
+        public void DemoMethod(T no)
+        {
+            Console.WriteLine(no);
+        }
+    }
+
+    public class Generics
+    {
+        public void GenericsMethod<T>(T num)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ DemoGenerics/DemoGenerics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Generics<int> generics=new Generics<int>(25);
+          Generics<string> ge=new Generics<string>("Manoj");
+          Generics<double> gen=new Generics<double>(25.5);
+          Generics<bool> gene=new Generics<bool>(true);
+            Console.WriteLine();
+
+            generics.DemoMethod(21);
+            ge.DemoMethod("Kumar");
+            gen.DemoMethod(26.555);
+            gene.DemoMethod(true);
+            Console.WriteLine();
+
+            Generics g=new Generics();
+            g.GenericsMethod(1);
+            g.GenericsMethod("Manoj");
+            g.GenericsMethod(37.88);
+            g.GenericsMethod<int>(2);
+            g.GenericsMethod<string>("jaswanth");
+            g.GenericsMethod<double>(37.88);
+            Console.ReadLine();
+        }
+    }
+}
